bdb591c6bffc77a268ade1135a918f378b7d85aa
Amir_Mohamad_Hassani <amhassani7669467@gmail.com>
2024-08-28 19:54:35 +0000

    header page one error  no image

---
+++ kasuka_page/src/App.js
@@ -0,0 +1,6 @@
+import Hed from "/home/avanger766/Documents/pagekasuka/kasuka_page/src/header/Headerpage.js"
+import "./App.css";
+function App() {
+  return <Hed/>;
+}
+export default App;
---
+++ kasuka_page/src/header/Headerpage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Hed() {
+  return (
+    <div className="headers">
+      <h1>
+        <a>
+          <img src="/home/avanger766/Documents/pagekasuka/kasuka_page/public/image/logo.png" alt="logo" />
+          kasuka
+        </a>
+      </h1>
+      <nav>
+        <ul>
+          <li>خانه</li>
+          <li>درباره ما</li>
+          <li>خدمات</li>
+          <li>نمونه کارها</li>
+          <li>تیم</li>
+          <li>لیست کشویی</li>
+          <li> تماس با ما</li>
+        </ul>
+      </nav>
+      <a href="">شروع</a>
+    </div>
+  );
+}
