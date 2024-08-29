1fdf2041630aeea514616481c39a2dc6fb2a6425
Amir_Mohamad_Hassani <amhassani7669467@gmail.com>
2024-08-29 20:29:32 +0000

    add stayle of ul>li and change the direction to center

--- kasuka_page/src/header/Headerpage.js
+++ kasuka_page/src/header/Headerpage.js
@@ -1,28 +1,40 @@
 import React from "react";
-import "/home/avanger766/Documents/pagekasuka/kasuka_page/src/header/headerpage.css"
-
+import "/home/avanger766/Documents/pagekasuka/kasuka_page/src/header/headerpage.css";
 
 export default function Hed() {
   return (
     <div className="headers">
       <h1>
         <a>
-           <img src="kasuka_page/public/image/logo.png" alt=""/>
-           kasuka
+          <img src="kasuka_page/src/header/logo.png" alt="logo" /> kasuka
         </a>
       </h1>
       <nav>
         <ul>
-          <li>خانه</li>
-          <li>درباره ما</li>
-          <li>خدمات</li>
-          <li>نمونه کارها</li>
-          <li>تیم</li>
-          <li>لیست کشویی</li>
-          <li> تماس با ما</li>
+          <li>
+            <a className="link-menu" href="">خانه</a>
+          </li>
+          <li>
+            <a className="link-menu" href="">درباره ما</a>
+          </li>
+
+          <li>
+            <a className="link-menu" href="">خدمات</a>
+          </li>
+          <li>
+            <a  className="link-menu"href="">نمونه کارها</a>
+          </li>
+          <li>
+            <a className="link-menu" href="">لیست کشویی</a>
+          </li>
+          <li>
+            <a className="link-menu" href="">تماس با ما</a>
+          </li>
         </ul>
       </nav>
-      <a href="">شروع</a>
+      <a className="link" href="">
+        شروع
+      </a>
     </div>
   );
 }
